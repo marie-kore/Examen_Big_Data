953c02645d7faf9b99a8e9e198a4d805091c625c
@marie-kore <estherkore75@gmail.com>
2021-05-20 13:12:44 +0000

    seconde fonction

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -1,10 +1,25 @@
 object ExamenBigData {
   def main(args:Array[String]): Unit = {
-    /*def renvoi():List[String]= {}*/
+    def renvoi():List[String]= {
+      val maliste : List[String] = List("julien","Paul","Jean","rac", "trec","joel","ed","chris")
+      val renvoi : List[String] = maliste.filter(_.reverse.take(1) == "n" )
+      renvoi.foreach(println)
+    }
+    def liste_double() : Unit = {
 
-    val maliste : List[String] = List("Nulien","Naomie","aristote","Nupil", "Richard")
-    val renvoi : List[String] = maliste.filter(_.reverse.take(1) == "n" )
-    renvoi.foreach(println)
+    }
+    val maliste2 : List[String] =  Array(Array("ecommercemag.fr"),
+      Array("https://www.journalducm.com/contact/", "payant"),
+      Array("https://www.zatsaz.com/"),
+      Array("https://www.lerevenu.com/"),
+      Array("https://www.cadre-dirigfdeant-magazine.com/", "payant"));
+
+    val listerecup :List[String] = maliste2.filter(maliste2.substring(0, maliste2.length - 1))
+    listerecup.foreach(println)
+
+
+
+
   }
 
 }
