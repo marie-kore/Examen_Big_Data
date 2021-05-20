040a226558e77e334d34c361f1047875c68483c0
@marie-kore <estherkore75@gmail.com>
2021-05-20 14:58:39 +0000

    seconde fonction 2

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -1,25 +1,29 @@
 object ExamenBigData {
   def main(args:Array[String]): Unit = {
-    def renvoi():List[String]= {
-      val maliste : List[String] = List("julien","Paul","Jean","rac", "trec","joel","ed","chris")
-      val renvoi : List[String] = maliste.filter(_.reverse.take(1) == "n" )
-      renvoi.foreach(println)
+
+
+  def renvoi(maliste : List[String]):List[String]= {
+    val maliste : List[String] = List("julien","Paul","Jean","rac", "trec","joel","ed","chris")
+    val renvoi : List[String] = maliste.filter(_.reverse.take(1) == "n" )
+    return renvoi
     }
-    def liste_double() : Unit = {
+    def tableau() : Unit = {
 
+      val listetableau = List(("ecommercemag.fr"," "),
+        ("https://www.journalducm.com/contact/","Payant"),
+        ("https://www.zatsaz.com/"," "),
+        ("https://www.lerevenu.com/"," "),
+        ("https://www.cadre-dirigfdeant-magazine.com/","Payant"),
+        ("https://www.silicon.fr/services/contact#annoner","Payant"),
+        ("https://www.channelbiz.fr/nous-contacter/"," "),
+        ("https://www.itespresso.fr/"," "),
+        ("https://www.industrie-mag.com/article4.html","invite"),
+        ("https://www.jesuisundev.com/article-invite/","invite"),
+        ("https://www.numerama.com/"," ")
+        )
+      val listefin = (Map(listetableau:_*).keys.toList.map(l => l.replace("https://www.","").split("/")(0) ) zip Map(listetableau:_*).values).toList
     }
-    val maliste2 : List[String] =  Array(Array("ecommercemag.fr"),
-      Array("https://www.journalducm.com/contact/", "payant"),
-      Array("https://www.zatsaz.com/"),
-      Array("https://www.lerevenu.com/"),
-      Array("https://www.cadre-dirigfdeant-magazine.com/", "payant"));
-
-    val listerecup :List[String] = maliste2.filter(maliste2.substring(0, maliste2.length - 1))
-    listerecup.foreach(println)
-
-
-
-
   }
 
 }
+
